df40b69dd977d68452f39b4d1f1a1905760d9d0a
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-10 20:31:26 +0300

    Logic was added to the post route to save data to database

--- GraceNakazibwe/routes/patientRegRoutes.js
+++ GraceNakazibwe/routes/patientRegRoutes.js
@@ -1,12 +1,31 @@
+/* eslint-disable no-console */
+/* eslint-disable no-empty */
 /* eslint-disable no-unused-vars */
 const express = require('express');
 
 const router = express.Router();
 const mongoose = require('mongoose');
+const PatientsReg = require('../models/patientRegModels');
 
 // Route to the registration page.
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const patientsReg = new PatientsReg(req.body);
+    await patientsReg.save((err) => {
+      // console.log(patientsReg)
+      if (err) {
+        throw err;
+      }
+      res.redirect('/')
+    });
+  } catch (error) {
+    res.status(400).send('Sorry! Data was not sent to DB');
+    console.log(error);
+  }
 });
 
 module.exports = router;
