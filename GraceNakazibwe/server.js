ef1c3aa26cd4cff092b3ec81361c183061067338
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2021-11-10 13:13:34 +0300

    Mongoose conection was added to server

--- GraceNakazibwe/server.js
+++ GraceNakazibwe/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-multiple-empty-lines */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable prefer-template */
@@ -10,6 +11,26 @@ const path = require('path');
 
 const app = express();
 
+require('dotenv').config();
+
+// Database connection.
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection now  open');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+// Requiring routes.
+const PatientReg = require('./routes/patientRegRoutes');
+
 // Configurations.
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname + '/views'));
@@ -20,10 +41,9 @@ app.use(express.static(path.join(__dirname + '/public')));
 app.use('/public/imagefiles', express.static(__dirname + '/public/imagefiles'));
 
 // Routes for the project.
-app.get('/', (req, res) => {
-//   res.render('index');
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.use('/', PatientReg);
+
+
 
 app.listen(port, () => {
   console.log(`We are listening on port ${port}`);
