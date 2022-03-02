ef89df6b265b3db5fd95e3b9b67a53cd4292ceba
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-03-02 20:26:56 +0300

    Template files moved

--- GraceNakazibwetemp/models/templateModels.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-const express = require('express');
-const mongoose = require('mongoose');
-
-// Project Schema.
-const templateSchema = new mongoose.Schema({
-
-});
-module.exports = mongoose.model('TemplateReg', templateSchema);
